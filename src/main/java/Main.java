27d8dce1f34fe2cb708e786b11621f9216358b6b
BartoszPatro <161605900+BartoszPatro@users.noreply.github.com>
2024-03-13 13:36:50 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) 
+  {
+   String nazwa="Hello world!";
+    for (int i=0; i<nazwa.length(); i++)
+      {
+        System.out.println(nazwa.charAt(i));
+      }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
